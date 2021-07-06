0e70d57ee5f8084c391e52b0dd711c74ab50652e
Alexander Nikolaev <quattro.ex@gmail.com>
2021-07-06 12:57:13 +0300

    del comments

--- src/js/modules/getData.js
+++ src/js/modules/getData.js
@@ -2,7 +2,7 @@ import { startDownloadData } from './render.js';
 
 const searchForm = document.forms.searchCityForm;
 const btnSearchCity = document.getElementById('btn-search-city');
-const APY_KEY = 'f1ed970e28bdb4d0cc27c77b0366d717';
+const APY_KEY = 'YOUR_API_KEY on the openweathermap.org';
 let cityName = 'Москва';
 let cityDataUrl = `https://nominatim.openstreetmap.org/search.php?q=${cityName}&format=json&addressdetails=1&limit=1`;
 
@@ -25,7 +25,6 @@ btnSearchCity.addEventListener('click', (e) => {
   e.preventDefault();
   const inputValue = new FormData(searchForm);
   cityName = inputValue.get('searchCity');
-  // cityName = checkCityName(city);
   cityDataUrl = `https://nominatim.openstreetmap.org/search.php?q=${cityName}&format=json&addressdetails=1&limit=1`;
   startDownloadData();
   searchForm.reset();
